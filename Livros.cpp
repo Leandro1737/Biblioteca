9425816f52e908e79886f591af515da53b630a55
Leandro1737 <leandromacielhti2016@gmail.com>
2024-10-29 14:25:55 -0300

    Criado as struct  livros e emprestimo

---
+++ Livros.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+    struct livros{
+        int codigo;
+        string titulo, autor;
+};
+    struct emprestimo {
+        int codigoLivro;
+        string usuario,dataEmprestimo,dataDevolucao;
+};
+
+int main(){
+
+}
