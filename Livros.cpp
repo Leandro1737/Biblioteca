070caeb2fca2cfc7b454e9464dfe828e24e320f7
Leandro1737 <leandromacielhti2016@gmail.com>
2024-10-29 14:34:51 -0300

    criado o início do menu de livros

--- Livros.cpp
+++ Livros.cpp
@@ -11,6 +11,19 @@ using namespace std;
         string usuario,dataEmprestimo,dataDevolucao;
 };
 
+void exibirMenu() {
+    cout << "\n--- Menu de Livros ---" << endl;
+    cout << "1 – Cadastro" << endl;
+    cout << "2 – Alteracao" << endl;
+    cout << "3 – Exclusao" << endl;
+    cout << "4 – Emprestimo" << endl;
+    cout << "5 – Devolucao" << endl;
+    cout << "6 – Consulta de livro" << endl;
+    cout << "7 – Livros disponiveis" << endl;
+    cout << "8 – Listagem geral de livros" << endl;
+    cout << "9 – Sair" << endl;
+    cout << "Escolha uma opcao: ";
+};
 int main(){
 
 }
